ad84e751148f13f06fdfaf570eb27f740f19837d
diasyago <38273222+diasyago@users.noreply.github.com>
2023-05-04 13:17:42 -0300

    Update animate.js

--- scripts/macros/animate.js
+++ scripts/macros/animate.js
@@ -1,25 +1,23 @@
 var hasSurged = actor.getFlag("wild-magic-surge-5e", "hassurged");
-
+console.log(hasSurged);
 if (hasSurged) {
   new Sequence("WMSTRUE")
     .effect()
-    .file(
-      "modules/JB2A_DnD5e/Library/Generic/Nature/SwirlingLeavesComplete01_02_Regular_Green_400x400.webm"
-    )
+    .file("modules/JB2A_DnD5e/Library/Generic/Marker/EnergyStrands_01_Regular_Blue_600x600.webm")
     .duration(10000)
     .fadeIn(500)
     .fadeOut(1000)
     .atLocation(token.id)
+    .sound("EXTERNAL%20FILES/sons/magias/sussuros.ogg")  
     .play();
 } else {
-  new Sequence("WMSFALSE")
+  new Sequence("WMSTRUE")
     .effect()
-    .file(
-      "modules/JB2A_DnD5e/Library/Generic/Particles/ParticlesOutward02_03_Regular_GreenYellow_400x400.webm"
-    )
+    .file("modules/JB2A_DnD5e/Library/Generic/Marker/EnergyStrands_01_Regular_Blue_600x600.webm")
     .duration(10000)
     .fadeIn(500)
     .fadeOut(1000)
     .atLocation(token.id)
+    .sound("EXTERNAL%20FILES/sons/magias/sussuros.ogg") 
     .play();
 }
